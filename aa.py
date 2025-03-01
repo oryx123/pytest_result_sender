f4303926e2d4845928b012d7130c6c542f2482d9
oryx123 <1073889494@qq.com>
2025-03-01 11:21:07 +0800

    Signed-off-by: oryx123 <1073889494@qq.com>

---
+++ aa.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+import pytest
+
+
+def add(a, b):
+    c = a + b
+
+    return 0
+
+
+add(1, 2)
--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
+
 def pytest_configure():
     # 配置加载完毕，测试用例之前
 
     print(f"{datetime.now()}: 开始执行测试用例")
+
 
 def pytest_unconfigure():
     # 结束测试
